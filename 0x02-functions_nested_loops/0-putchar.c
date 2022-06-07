62a39494c618388a54ffad2122421dec1c0cc7a7
Wittyambivert <wittyambivert@gmail.com>
2022-06-07 10:08:29 -0700

    hotfix for 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the word _putchar
  *
- * Return: Alswas 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 
 int main(void)
 {
-	printf("_putchar\n");
+	char word[8] = "_putchar";
+	int i = 0;
+
+	while (i <= 7)
+	{
+		putchar(word[i]);
+		putchar('\n');
+	}
+
+
+
 	return (0);
 }
