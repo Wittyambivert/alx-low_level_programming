67f9520829ba4e537029953d34d7221b4223e0ac
Wittyambivert <wittyambivert@gmail.com>
2022-06-07 10:47:44 -0700

    added 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print_alphabet(void);
+
+int main(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= z; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+
+
+
+	return (0);
+}
