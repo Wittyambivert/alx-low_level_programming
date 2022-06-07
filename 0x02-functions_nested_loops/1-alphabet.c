985d991850f8ead177787ea8f8000228d4b7a3d1
Wittyambivert <wittyambivert@gmail.com>
2022-06-07 11:29:16 -0700

    tried to make betty to stop trying

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 
+
+/**
+ * print_alphabet - function to print alphabet in lower case
+ *
+ * Description: uses function prototype
+ * Return 0 success
+ */
+
+
+
+
 void print_alphabet(void);
 
 int main(void)
 {
+	print_alphabet();
+	return (0);
+}
+
+
+void print_alphabet(void)
+{
 	char alpha;
-	for (alpha = 'a'; alpha <= z; alpha++)
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		putchar(alpha);
 	}
 	putchar('\n');
-
-
-
-	return (0);
 }
