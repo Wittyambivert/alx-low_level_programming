51fddc752b30fac4b380252e10d813bc257bb20c
Wittyambivert <wittyambivert@gmail.com>
2022-06-08 10:53:48 -0700

    added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - print the sign of integer
+ * @n: - added for betty
+ * Return: 0
+ */
+
+
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	else
+	{
+		printf("-1");
+		return (-1);
+	}
+}
