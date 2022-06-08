f5bdfb4e840f74348be544212f99ef43ec51b8f4
Wittyambivert <wittyambivert@gmail.com>
2022-06-08 12:19:08 -0700

    added 7-print_last_digit.c file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @last: for betty
+ * Return: 0
+ */
+
+
+int print_last_digit(int last)
+{
+	_putchar( last % 10);
+	return (last % 10);
+}
