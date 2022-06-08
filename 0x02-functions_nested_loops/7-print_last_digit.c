2b71e585cf58f79c0a100e108de462657d81da34
Wittyambivert <wittyambivert@gmail.com>
2022-06-08 15:11:10 -0700

    fixing 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,19 @@
 
 int print_last_digit(int last)
 {
-	int printlast = last % 10;
 
-	putchar(printlast + '0');
+	if (last < 0)
+	{
+		printlast = (-1 * (last % 10));
+		putchar(printlast + '0');
+		return (printlast);
+	}
+	else
+	{
+		printlast = last % 10;
+		_putchar(printlast + '0');
+		return (printlast);
+	}
 
-	return (printlast);
+
 }
