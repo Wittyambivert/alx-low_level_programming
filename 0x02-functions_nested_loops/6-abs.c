c7aa0e932f5c54e0c34c4f1ce3afb6cf13d54840
Wittyambivert <wittyambivert@gmail.com>
2022-06-08 11:24:39 -0700

    added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - function to find absolute value of integer
+ * @c: for betty
+ * Return: 0
+ */
+
+
+int _abs(int c)
+{
+	printf("%d", abs(c));
+
+	return (0);
+}
