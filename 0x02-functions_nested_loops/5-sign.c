9a0343992ffaf5f2884723e2f87f87bbab905d94
Wittyambivert <wittyambivert@gmail.com>
2022-06-08 12:11:05 -0700

    fixing 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,17 @@ int print_sign(int n)
 {
 	if (n > 1)
 	{
-		return (-1);
+		_putchar(1);
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return (00);
+		_putchar(0);
+		return (0);
 	}
 	else
 	{
+		_putchar(-1);
 		return (-1);
 	}
 }
