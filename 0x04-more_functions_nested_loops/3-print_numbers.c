fd2dae6d617de9baaf73cfcdf2c48f9e6c701438
Wittyambivert <wittyambivert@gmail.com>
2022-06-10 12:14:35 -0700

    added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print numbers from 0-9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
