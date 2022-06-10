ccd61332fd620ea364bc9b88105e1e08b6e2c941
Wittyambivert <wittyambivert@gmail.com>
2022-06-10 13:59:53 -0700

    added 5-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line on the terminal using only putchar
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	char i;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
