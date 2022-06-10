c72f7171fac1e32334f68be6f1da359e15f65980
Wittyambivert <wittyambivert@gmail.com>
2022-06-09 17:26:55 -0700

    added 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+
+
+/**
+ * _isupper - function to print uppercase check
+ * @c: - for betty
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
