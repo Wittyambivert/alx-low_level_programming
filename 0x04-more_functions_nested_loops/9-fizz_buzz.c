a697c514f64ffefdc013cb2bae98f4dd4d3fa121
Wittyambivert <wittyambivert@gmail.com>
2022-06-11 01:26:41 -0700

    fixing 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 99; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -29,7 +29,7 @@ int main(void)
 			printf("%i ", i);
 		}
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
