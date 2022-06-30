f649108d076d14eb37a0e1f8d060eb553cb64814
wittyambivert <wittyambivert@gmail.com>
2022-06-29 21:12:02 -0700

    hot fix

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the word _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i = 0;
+
+	while (i <= 7)
+	{
+		putchar(word[i]);
+		i++;
+	}
+	putchar('\n');
+
+
+	return (0);
+}
